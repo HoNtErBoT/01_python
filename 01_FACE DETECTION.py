0cc686eb6c1526223e5c592889cde7cfe41ad0c4
Rajeev TR <109785046+HoNtErBoT@users.noreply.github.com>
2022-12-31 12:06:18 +0530

    Add files via upload

---
+++ 01_FACE DETECTION.py
@@ -0,0 +1,38 @@
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# To capture video from webcam.
+cap = cv2.VideoCapture(0)
+# To use a video file as input
+# cap = cv2.VideoCapture('filename.mp4')
+
+while True:
+    # Read the frame
+    _, img = cap.read()
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        X=int(x+(w/2))
+        Y=int(y+(h/2))
+        img = cv2.circle(img, (X, Y), radius=3, color=(0, 0, 255), thickness=-1)
+        print("loc = "+str(x)+" , " + str(y) + "width = "+str(w)+" height " + str(h)+ "width con= "+str(X)+" height con= " + str(Y))
+
+    # Display
+    cv2.imshow('img', img)
+
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+# Release the VideoCapture object
+cap.release()
